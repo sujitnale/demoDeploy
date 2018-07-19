c0664ede94ae8abfaa6641dea8776aba197ba46c
Sujit Nale <sujit.nale@globant.com>
2018-07-19 16:32:05 +0530

    ready for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './books/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/books/book.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  template: `
+    <p>
+      book works!
+    </p>
+  `,
+  styles: []
+})
+export class BookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
